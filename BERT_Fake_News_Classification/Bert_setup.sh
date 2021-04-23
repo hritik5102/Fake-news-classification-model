f41024b3e5876f6a71a415ffc10f4ce5bf575b6d
hritik5102 <hritik.jaiswal@somaiya.edu>
2021-04-24 00:46:20 +0530

    updated bert bash file

--- BERT_Fake_News_Classification/Bert_setup.sh
+++ BERT_Fake_News_Classification/Bert_setup.sh
@@ -1,7 +1,7 @@
-echo -e "Creating a model folder ...\n"
+echo -e "[INFO] Creating a model folder ...\n"
 mkdir -p .model .model/uncased_L-12_H-768_A-12
 
-echo -e "Downloading BERT Model ...\n"
+echo -e "[INFO] Downloading BERT Model ...\n"
 gsutil cp gs://bert_models/2018_10_18/uncased_L-12_H-768_A-12/bert_config.json .model/uncased_L-12_H-768_A-12
 gsutil cp gs://bert_models/2018_10_18/uncased_L-12_H-768_A-12/vocab.txt .model/uncased_L-12_H-768_A-12
 gsutil cp gs://bert_models/2018_10_18/uncased_L-12_H-768_A-12/bert_model.ckpt.meta .model/uncased_L-12_H-768_A-12
@@ -10,23 +10,10 @@ gsutil cp gs://bert_models/2018_10_18/uncased_L-12_H-768_A-12/bert_model.ckpt.da
 
 ls -la .model .model/uncased_L-12_H-768_A-12
 
-echo -e "Downloading Pretrained weights ...\n"
+echo -e "[INFO] Downloading Pretrained weights ...\n"
 
-mkdir pretrained_model 
+mkdir pretrained_weights
 
-wget https://www.dropbox.com/s/7vf4oqti3abvbz4/bert_news.h5 -P /pretrained_model
+gdown --id 1pI12nFTsNdcoAY-tEyuybuYgP8kKOILA
 
-
-# mkdir pretrained_model 
-
-# sudo wget https://www.dropbox.com/s/7vf4oqti3abvbz4/bert_news.h5 -P /pretrained_model
-
-mkdir pretrained_model 
-sudo wget https://www.dropbox.com/s/7vf4oqti3abvbz4/bert_news.h5  --directory-prefix  ~/pretrained_model
-
-
-# mkdir pretrained_model 
-
-# gdown --id 1pI12nFTsNdcoAY-tEyuybuYgP8kKOILA
-
-# mv bert_news.h5 ~/pretrained_model
+mv bert_news.h5 pretrained_weights
