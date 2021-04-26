7b2e7fc0279ef017aeeded0bf5f99d8d97bb6752
Heet Sakaria <heet9022@users.noreply.github.com>
2021-04-27 02:57:55 +0530

    The setup file doesn't work. Adding colab req just for reference.Don't panic

--- setup.sh
+++ setup.sh
@@ -9,8 +9,8 @@ enableCORS = false\n\
 \n\
 " > ~/.streamlit/config.toml
 
-# echo -e "[INFO] Running BERT bash script  ...\n"
-# BERT_Fake_News_Classification/Bert_setup.sh
+echo -e "[INFO] Running BERT bash script  ...\n"
+BERT_Fake_News_Classification/Bert_setup.sh
 
 # echo -e "[INFO] Running GPT2 bash script  ...\n"
 # GPT2_Model/GPT_setup.sh
