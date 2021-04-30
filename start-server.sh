491fe2c45e5add8f72a7efa3ad6e561a660ad2b3
Heet Sakaria <heet9022@users.noreply.github.com>
2021-04-30 21:57:56 +0530

    Added bash files to start and stop the model servers

--- start-server.sh
+++ start-server.sh
@@ -1,3 +1,8 @@
-nohup python Server-GPT2/gpt-server.py &
-nohup python Server-LSTM/lstm-server.py &
-nohup python Server-BERT/bert-server.py &
+# Run this in Git Bash or any linux terminal
+
+nohup python Server-GPT2/gpt-server.py > gpt.log 2>&1 &
+echo $! >> save_pid.txt
+nohup python Server-LSTM/lstm-server.py > lstm.log 2>&1 &
+echo $! >> save_pid.txt
+nohup python Server-BERT/bert-server.py > bert.log 2>&1 &
+echo $! >> save_pid.txt
